077d57b85fcee774f3bd8742f76fe819cb53450c
RodrigoPessoaDev <rodrigopessoa.dev@gmail.com>
2023-11-05 17:05:15 +0000

    Implement hover effect on basic grid

--- script.js
+++ script.js
@@ -3,9 +3,17 @@ const container = document.querySelector("#containerSketch");
 function makeGrid(rows, cols) {
   for (i = 0; i < rows * cols; i++) {
     let item = document.createElement("div");
-    item.innerText = i + 1;
+    //item.innerText = i + 1;
     container.appendChild(item).className = "grid-item";
   }
 }
 
 makeGrid(16, 16);
+
+const itemGrid = document.querySelectorAll(".grid-item");
+
+[...itemGrid].forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    item.classList.add("item-color");
+  });
+});
