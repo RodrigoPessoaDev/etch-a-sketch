5fdf9cbfb4a96ce115fe9f7f2261aa58dde39809
RodrigoPessoaDev <rodrigopessoa.dev@gmail.com>
2023-11-07 13:52:26 +0000

    Fix bug that did not allow to change back color

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const container = document.querySelector("#containerSketch");
+const btn32 = document.querySelector(".btn32");
+const btn64 = document.querySelector(".btn64");
+const reset = document.querySelector(".btnReset");
+const colorChange = document.querySelector(".btnColor");
+const btnBlack = document.querySelector(".btnBlack");
+const btnBlue = document.querySelector(".btnBlue");
 
 function makeGrid(rows, cols) {
   for (i = 0; i < rows * cols; i++) {
@@ -8,12 +14,39 @@ function makeGrid(rows, cols) {
   }
 }
 
-makeGrid(40, 40);
+function clearGrid() {
+  container.removeChild([...item]);
+}
 
+makeGrid(40, 40);
 const itemGrid = document.querySelectorAll(".grid-item");
 
 [...itemGrid].forEach((item) => {
   item.addEventListener("mouseover", () => {
     item.classList.add("item-color");
+  });
+});
+
+reset.addEventListener("click", () => {
+  [...itemGrid].forEach((item) => {
+    item.classList.remove("item-color", "item-black");
+  });
+});
+
+btnBlack.addEventListener("click", () => {
+  [...itemGrid].forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.classList.add("item-black");
+      item.classList.remove("item-blue");
+    });
+  });
+});
+
+btnBlue.addEventListener("click", () => {
+  [...itemGrid].forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.classList.add("item-blue");
+      item.classList.remove("item-black");
+    });
   });
 });
