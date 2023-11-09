21090c960654ebad2d9e17cbe8da4af1ee2814a3
RodrigoPessoaDev <rodrigopessoa.dev@gmail.com>
2023-11-09 19:02:15 +0000

    Refactor all code to get flexbox to work on all grid sizes

--- script.js
+++ script.js
@@ -1,55 +1,104 @@
-const container = document.querySelector("#containerSketch");
-const btn32 = document.querySelector(".btn32");
-const btn64 = document.querySelector(".btn64");
-const reset = document.querySelector(".btnReset");
-const colorChange = document.querySelector(".btnColor");
-const btnBlack = document.querySelector(".btnBlack");
-const btnBlue = document.querySelector(".btnBlue");
+let btnReset = document.querySelector(".btn-reset");
+let btnRandom = document.querySelector(".btn-random");
+let btnPink = document.querySelector(".btn-pink");
+let gridContainer = document.querySelector(".container");
+let totalNumberSquares = 2500;
+let gridItem;
+let userSquares;
+let itemPx;
+let allGridItems;
 
-function makeGrid(rows, cols) {
-  for (i = 0; i < rows * cols; i++) {
-    let item = document.createElement("div");
-
-    container.appendChild(item).className = "grid-item";
+//Generate a grid
+function makeGrid() {
+  for (i = 0; i < totalNumberSquares; i++) {
+    gridItem = document.createElement("div");
+    gridContainer.appendChild(gridItem);
   }
 }
 
+//Reset Grid
 function clearGrid() {
-  [...itemGrid].forEach((item) => {
-    container.removeChild(item);
-  });
+  while (gridContainer.hasChildNodes()) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
 }
 
-makeGrid(40, 40);
-const itemGrid = document.querySelectorAll(".grid-item");
+//Add event handler
+function mouseOverEvent() {
+  allGridItems = document.querySelectorAll(".container div");
+  for (let i = 0; i < allGridItems.length; i++) {
+    allGridItems[i].addEventListener("mouseover", setBlackColour);
+  }
+}
 
-//Color chaning buttons
-[...itemGrid].forEach((item) => {
-  item.addEventListener("mouseover", () => {
-    item.classList.add("item-color");
-  });
-});
+//Color black
+function setBlackColour() {
+  this.style.backgroundColor = "black";
+}
 
-reset.addEventListener("click", () => {
-  [...itemGrid].forEach((item) => {
-    item.classList.remove("item-color", "item-black");
-  });
-});
+//Resize item
+function itemSize() {
+  itemPx = 500 / userSquares + "px";
+  for (let i = 0; i < allGridItems.length; i++) {
+    allGridItems[i].style.flexBasis = itemPx;
+  }
+}
 
-btnBlack.addEventListener("click", () => {
-  [...itemGrid].forEach((item) => {
-    item.addEventListener("mouseover", () => {
-      item.classList.add("item-black");
-      item.classList.remove("item-blue");
-    });
-  });
-});
+function resetGrid() {
+  for (let i = 0; i < allGridItems.length; i++) {
+    allGridItems[i].style.backgroundColor = "white";
+  }
+  userSquares = prompt("How many squares per side? (Min : 8, Max:100", "100");
+  if (userSquares == 0) {
+    userSquares = 50;
+  } else if (userSquares > 100) {
+    userSquares = 100;
+  } else if (userSquares < 8) {
+    userSquares = 8;
+  }
+  totalNumberSquares = userSquares * userSquares;
+  clearGrid();
+  makeGrid();
+  mouseOverEvent();
+  itemSize();
+}
 
-btnBlue.addEventListener("click", () => {
-  [...itemGrid].forEach((item) => {
-    item.addEventListener("mouseover", () => {
-      item.classList.add("item-blue");
-      item.classList.remove("item-black");
-    });
-  });
-});
+function switchToColours() {
+  allGridItems = document.querySelectorAll(".container div");
+
+  for (let i = 0; i < allGridItems.length; i++) {
+    allGridItems[i].removeEventListener("mouseover", setBlackColour);
+    allGridItems[i].addEventListener("mouseover", setRandomColour);
+  }
+}
+
+function setPinkColor() {
+  this.style.backgroundColor = "pink";
+}
+
+function switchToPink() {
+  allGridItems = document.querySelectorAll(".container div");
+
+  for (let i = 0; i < allGridItems.length; i++) {
+    allGridItems[i].removeEventListener("mouseover", setBlackColour);
+    allGridItems[i].removeEventListener("mouseover", setRandomColour);
+    allGridItems[i].addEventListener("mouseover", setPinkColor);
+  }
+}
+
+function random_rbg() {
+  let o = Math.round,
+    r = Math.random,
+    s = 255;
+  return "rgb(" + o(r() * s) + "," + o(r() * s) + "," + o(r() * s) + ")";
+}
+
+function setRandomColour() {
+  this.style.backgroundColor = random_rbg();
+}
+
+makeGrid();
+mouseOverEvent();
+btnReset.addEventListener("click", resetGrid);
+btnRandom.addEventListener("click", switchToColours);
+btnPink.addEventListener("click", switchToPink);
